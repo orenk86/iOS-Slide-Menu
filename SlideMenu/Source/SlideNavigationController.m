c5651a453eec790021b9bb7e99d7cc6340a803a0
Oren Kosto <oren@panda-os.com>
2016-04-18 15:22:39 +0300

    fixed black screen issue
    Commented out lines that prevent the menu from being initialized in some cases, causing a black screen to appear instead of the menu.

--- SlideMenu/Source/SlideNavigationController.m
+++ SlideMenu/Source/SlideNavigationController.m
@@ -591,8 +591,8 @@ static SlideNavigationController *singletonInstance;
 - (void)prepareMenuForReveal:(Menu)menu
 {
 	// Only prepare menu if it has changed (ex: from MenuLeft to MenuRight or vice versa)
-    if (self.lastRevealedMenu && menu == self.lastRevealedMenu)
-        return;
+//    if (self.lastRevealedMenu && menu == self.lastRevealedMenu)
+//        return;
     
     UIViewController *menuViewController = (menu == MenuLeft) ? self.leftMenu : self.rightMenu;
 	UIViewController *removingMenuViewController = (menu == MenuLeft) ? self.rightMenu : self.leftMenu;
